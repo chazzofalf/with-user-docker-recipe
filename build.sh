f46c58d7184bcc98f79ed401abda000063657726
Charles Montgomery <charles.montgomery@charter.net>
2019-04-18 22:18:52 -0500

    Added docker-main submodule.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ../../docker-helper-functions.sh
+source docker-main/docker-helper-functions.sh
 docker-build-image-with-name chazzofalf/ubuntu-base-docker-recipe
 docker-smart-build-core "$@"
